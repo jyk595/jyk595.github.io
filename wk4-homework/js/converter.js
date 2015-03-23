fc94dc57e9e9dac1f2fb08de5d72c7cdce2541c9
jyk595 <jyk595@gmail.com>
2015-03-22 23:48:29 -0400

    changed class names and fixed big in js

--- wk4-homework/js/converter.js
+++ wk4-homework/js/converter.js
@@ -1,46 +1,41 @@
-function changeFarenheit() {
-	var degreeCelcius = document.getElementById("cdegree").value;
-	document.getElementById("fdegree").value = (degreeCelcius*9)/5+32;
+function changeFahrenheit() {
+	var degreeCelsius = document.getElementById("cdegree").value;
+	document.getElementById("fdegree").value = (degreeCelsius*9)/5+32;
+	changeImage(degreeCelsius);
 }
 
-document.getElementById("cdegree").oninput = changeFarenheit;
-
-function changeCelcius() {
-	var degreeFarenheit = document.getElementById("fdegree").value;
-	document.getElementById("cdegree").value = ((degreeFarenheit-32)*5)/9;
+function changeCelsius() {
+	var degreeFahrenheit = document.getElementById("fdegree").value;
+	var degreeCelsius = ((degreeFahrenheit-32)*5)/9
+	document.getElementById("cdegree").value = degreeCelsius;
+	changeImage(degreeCelsius);
 }
 
-document.getElementById("fdegree").oninput = changeCelcius;
 
-function changeImage () {
-	var degreeCelcius = document.getElementById("cdegree").value;
-	if (degreeCelcius<=10) {
+function changeImage (degreeCelsius) {
+	if (degreeCelsius<=10) {
 		document.body.style.backgroundImage = "url('images/mcginley10.jpg')";
 	}
-
-	else if (degreeCelcius>10 && degreeCelcius<=20) {
+	else if (degreeCelsius>10 && degreeCelsius<=20) {
 		document.body.style.backgroundImage = "url('images/mcginley20.jpg')";
 	}
-
-	else if (degreeCelcius>20 && degreeCelcius<=30) {
+	else if (degreeCelsius>20 && degreeCelsius<=30) {
 		document.body.style.backgroundImage = "url('images/mcginley30.jpg')";
 	}
-	
-	else if (degreeCelcius>30 && degreeCelcius<=40) {
+	else if (degreeCelsius>30 && degreeCelsius<=40) {
 		document.body.style.backgroundImage = "url('images/mcginley40.jpg')";
 	}
-
-	else if (degreeCelcius>40 && degreeCelcius<=50) {
+	else if (degreeCelsius>40 && degreeCelsius<=50) {
 		document.body.style.backgroundImage = "url('images/mcginley50.jpg')";
 	}
-
-	else if (degreeCelcius>50 && degreeCelcius<=80) {
+	else if (degreeCelsius>50 && degreeCelsius<=80) {
 		document.body.style.backgroundImage = "url('images/mcginley60.jpg')";
 	}
-
-	else if (degreeCelcius>80) {
+	else if (degreeCelsius>80) {
 		document.body.style.backgroundImage = "url('images/mcginley80.jpg')";
 	}
 }
 
-// document.getElementById("cdegree").oninput = changeImage;
+document.getElementById("cdegree").oninput = changeFahrenheit;
+document.getElementById("fdegree").oninput = changeCelsius;
+
